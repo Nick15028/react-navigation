96c45ab4c1e74f6be42e6937ba45bcb3bf0f2bf4
Nick Chang <rich0432@gmail.com>
2022-07-07 15:51:09 +0800

    practice AsyncStorage

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as StorageHelper from '../helpers/StorageHelper'
 
 export default function ProfileScreen(props) {
+
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        loadStorage()
+        console.log('useEffect')
+    }, [])
+
+    const loadStorage = async () => {
+        let nameGet = await StorageHelper.getMySetting('name')
+        console.log('loadStorage！')
+        if (nameGet) {// if(name!== null)
+            setName(nameGet)
+        }
+    }
+    const changeName = async () => {
+        try {
+            await StorageHelper.setMySetting('name', name)
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
     return (
         <View style={styles.container}>
             <Text>Profile Screen</Text>
             <StatusBar style="auto" />
+            <TextInput
+                maxLength={10}
+                style={{ height: 50, width: 300, borderWidth: 5, borderColor: 'darkgrey', backgroundColor: 'grey', fontSize: 28, textAlign: 'center', color: 'white' }}
+                onChangeText={(text) => setName(text)}
+                value={name}
+            />
+            <Text>Hello {name}!</Text>
             <Button
-                title='go to profile detail screen'
-                onPress={() => props.navigation.push('ProfileDetailScreen')}
+                title='設定名字'
+                onPress={() => changeName()}
             />
         </View>
     );
